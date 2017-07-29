fb4927023c6c79ae5907b80c7f99873ba6012bcb
Sebastian G. Marinescu <sebastian.marinescu@gmail.com>
2017-07-29 14:04:26 +0200

    Template Loading fix

--- library.js
+++ library.js
@@ -14,7 +14,7 @@
 
 	function loadWidgetTemplate(template, next) {
 		winston.info("Loading templateFile: " + template);
-        fs.readFile(path.resolve(__dirname, template), function (err, data) {
+        fs.readFile(path.resolve(__dirname, './public/templates/' + template), function (err, data) {
 			if (err) {
 				console.log(err.message);
 				return next(null, err);
@@ -42,7 +42,7 @@
 		},
 
 		defineWidgets: function(widgets, callback) {
-			loadWidgetTemplate('./templates/admin/singlepost.tpl', function(templateData) {
+			loadWidgetTemplate('./admin/singlepost.tpl', function(templateData) {
 				widgets = widgets.concat([
 					{
 						widget: "singlepost",
